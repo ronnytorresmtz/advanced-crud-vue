dcdf1346db17186699c34d89768f40feb4783dcf
ronny torres <ronnytorresmtz@gmail.com>
2017-09-04 08:24:10 -0500

    eliminate compareValue method

--- client/src/lib/General.js
+++ client/src/lib/General.js
@@ -32,19 +32,19 @@ GENERAL FUNCTION LIBRARY
     * This function is used as pararmeter in the
     * for Sort function.
     */
-    export function compareValues(key, order = 'asc') {
-      return (a, b) => {
-        const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
-        const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
-        let comparison = 0;
-        if (varA > varB) {
-          comparison = 1;
-        } else if (varA < varB) {
-          comparison = -1;
-        }
-        return (
-          (order === 'desc') ?
-          (comparison * -1) : comparison
-        );
-      };
-    }
+    // export function compareValues(key, order = 'asc') {
+    //   return (a, b) => {
+    //     const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+    //     const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+    //     let comparison = 0;
+    //     if (varA > varB) {
+    //       comparison = 1;
+    //     } else if (varA < varB) {
+    //       comparison = -1;
+    //     }
+    //     return (
+    //       (order === 'desc') ?
+    //       (comparison * -1) : comparison
+    //     );
+    //   };
+    // }
