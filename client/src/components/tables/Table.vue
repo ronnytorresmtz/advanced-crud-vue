100c90b5e7db39aa7e42743e8dd522cfb9d3aae6
ronny torres <ronnytorresmtz@gmail.com>
2017-09-09 18:56:20 -0500

    add choose field to display (show/hide) functionality with localstoreage

--- client/src/components/tables/Table.vue
+++ client/src/components/tables/Table.vue
@@ -8,48 +8,85 @@
      white-space: nowrap;
      overflow-x: auto;
      overflow-y: auto;
-     width:auto;
+     width:auto
   }
 
   .table-height{
-    height: 370px;
+    height: 340px;
+  }
+
+  .table-cell-hide {
+    display: none;
+  }
+
+  .table-cell-show {
+    display: 'tab-cell';
   }
 
   .btn-width {
     width: 60px;
   }
+  .icon-cog {
+    padding-right: 20px;
+    padding-bottom: 10px;
+  }
+  .dropdown-item-cog {
+    padding-top: 2px;
+    padding-left: 10px;
+  }
 
+  [v-cloak] {
+    display: none;
+  }
 
 </style>
 
 <template>
-
-  <div  class="table-responsive table-hscroll table-height">
-    <table id="table1" class="table table-condensed table-hover">
-      <th v-for="col in cols">
-        <a @click.prevent="sort($event)" style="cursor:pointer" :id="col.name">
-          <span :class="isOrderBy()" v-show="isFieldOrder(col.name)"></span>
-          {{ ts[col.label] }}
+  <div>
+    <div class="row icon-cog">
+      <div class="dropdown">
+        <a class="dropdown-toggle pull-right" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+          <span class="glyphicon glyphicon-cog"></span>
+          <!--span class="caret"></span-->
         </a>
-      </th>
-      <tbody>
-        <tr v-for="row in rows">
-          <td  v-for="(value, key) in row" @click.prevent="itemSelected(row)">
-            <span v-if="key=='deleted_at'" >
-              <span v-if="value==null">
-                  <span class="btn btn-xs btn-success btn-width"> {{ ts['active'] }} </span>
+        <br>
+        <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">
+          <h6 class="dropdown-header">{{ ts['ChooseAFieldToDisplay'] }}</h6>
+          <li v-for="col in cols" class="dropdown-item-cog">
+            <input type="checkbox":name="col.name" @change.stop.prevent="columnToDisplay($event)" :checked="col.display">
+            {{ ts[col.label] }} 
+            </input>
+         </li>
+        </ul>
+      </div>
+    </div>
+    <div  class="table-responsive table-hscroll table-height"> <!-- v-if="!loading"-->
+      <table id="table1" class="table table-hover">
+        <th v-for="(col, key) in cols" :class="`table-cell-${hideOrShowCell(col.name)}`">
+          <a @click.prevent="sort($event)" style="cursor:pointer" :id="col.name"> <!--v-if="col.display"-->
+            <span :class="isOrderBy()" v-show="isFieldOrder(col.name)"></span>
+            {{ ts[col.label] }} 
+          </a>
+        </th>
+        <tbody>
+          <tr v-for="row in rows">
+            <td v-for="(value, key) in row" @click.prevent="itemSelected(row)" :id="key" :class="`table-cell-${hideOrShowCell(key)}`" >
+              <span v-if="key=='deleted_at'" >
+                <span v-if="value==null" v>
+                    <span class="btn btn-xs btn-success btn-width"> {{ ts['active'] }} </span>
+                </span>
+                <span v-else>
+                    <span class="btn btn-xs btn-danger btn-width"> {{ ts['inactive'] }} </span>
+                </span>
               </span>
               <span v-else>
-                  <span class="btn btn-xs btn-danger btn-width"> {{ ts['inactive'] }} </span>
+                  {{value}}
               </span>
-            </span>
-            <span v-else >
-                {{value}}
-            </span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 
 </template>
@@ -72,7 +109,33 @@ export default {
     };
   },
 
+  updated() {
+    const colsInfo = JSON.parse(localStorage.getItem('colsInfo'));
+    Object.keys(colsInfo).forEach((key) => {
+      this.cols[key].display = colsInfo[key].display;
+    });
+  },
+
   methods: {
+    hideOrShowCell(key) {
+      let action = 'hide';
+      Object.keys(this.cols).forEach((col) => {
+        if (this.cols[col].name === key) {
+          if (this.cols[col].display) {
+            action = 'show';
+          }
+        }
+      });
+      return action;
+    },
+    columnToDisplay(e) {
+      Object.keys(this.cols).forEach((col) => {
+        if (this.cols[col].name === e.target.name) {
+          this.cols[col].display = !this.cols[col].display;
+        }
+      });
+      this.storeInLocalStorage('colsInfo', this.cols);
+    },
     itemSelected(row) {
       const dataRow = this.$store.getters.getPageData.filter(item => item.id === row.id);
       store.commit('UPDATE_ITEM', createObj(dataRow[0]));
@@ -84,7 +147,6 @@ export default {
     },
     sort(e) {
       const fieldOrderBy = e.target.id;
-      console.log(fieldOrderBy);
       const orderBy = (this.$store.getters.getOrderBy === 'asc' && fieldOrderBy === this.$store.getters.getFieldOrderBy) ? 'desc' : 'asc';
       store.commit('UPDATE_ORDER_BY', orderBy);
       store.commit('UPDATE_FIELD_ORDER_BY', fieldOrderBy);
@@ -98,6 +160,11 @@ export default {
         return 'glyphicon glyphicon-sort-by-alphabet';
       }
       return 'glyphicon glyphicon-sort-by-alphabet-alt';
+    },
+    storeInLocalStorage(key, value) {
+      if (typeof (Storage) !== 'undefined') {
+        localStorage.setItem(key, JSON.stringify(value));
+      }
     },
   },
 };
