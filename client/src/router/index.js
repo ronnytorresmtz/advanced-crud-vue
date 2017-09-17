9a6d900f13ecd83b5b1c108e480457b50ca6c208
ronny torres <ronnytorresmtz@gmail.com>
2017-09-16 19:13:20 -0500

    delete hello route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/pages/Hello';
 import Companies from '@/pages/Companies';
 
 Vue.use(Router);
@@ -8,11 +7,6 @@ Vue.use(Router);
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello,
-    },
     {
       path: '/companies',
       name: 'Companies',
