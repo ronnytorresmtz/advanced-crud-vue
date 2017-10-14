de4b0a917b517f9ec477d236d81361342ad3ae08
ronny torres <ronnytorresmtz@gmail.com>
2017-10-14 15:49:47 -0500

    clean code

--- client/src/components/layout/topbar.vue
+++ client/src/components/layout/topbar.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    <nav class="navbar navbar-inverse navbar-fixed-top" >
-     <div class="container-fluid">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
         <div class="navbar-header">
-          <a class="navbar-brand" href="#">WebSiteName</a>
+          <a class="navbar-brand" href="#">BrandName</a>
         </div>
-        <!--ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">Page 1</a></li>
-          <li><a href="#">Page 2</a></li>
-          <li><a href="#">Page 3</a></li>
-        </ul-->
-     </div>
+      </div>
     </nav>
   </div>
 </template>
+
+<script>
+
+  export default {
+
+    data() {
+      return {
+      };
+    },
+  };
+</script>
