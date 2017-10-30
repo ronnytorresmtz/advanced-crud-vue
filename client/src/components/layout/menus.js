0f3c11659b46527135af642e8ae4c0ebced04bd8
ronny torres <ronnytorresmtz@gmail.com>
2017-10-29 19:46:57 -0600

    update menus options

--- client/src/components/layout/menus.js
+++ client/src/components/layout/menus.js
@@ -3,23 +3,6 @@ export default [
     name: 'Dashboard',
     link: '/shipper/dashboard',
     icon: 'fa fa-tachometer',
-    child: [
-      {
-        name: 'Orders',
-        link: '/shipper/dashboard/orders',
-        icon: 'fa fa-bar-chart',
-      },
-      {
-        name: 'Payments',
-        link: '/shipper/dashboard/payments',
-        icon: 'fa fa-area-chart',
-      },
-      {
-        name: 'Evaluations',
-        link: '/shipper/dashboard/evaluations',
-        icon: 'fa fa-pie-chart',
-      },
-    ],
   },
   {
     name: 'Shippers',
@@ -36,82 +19,16 @@ export default [
         link: '/customers',
         icon: 'fa fa-user',
       },
-      {
-        name: 'Warehouses',
-        link: '/shipper/shippers/warehouses',
-        icon: 'fa fa-cube',
-      },
-    ],
-  },
-  {
-    name: 'Carries',
-    link: '#',
-    icon: 'fa fa-truck',
-    child: [
-      {
-        name: 'Invitations',
-        link: '/shipper/carriers/invitations',
-        icon: 'fa fa-envelope',
-      },
-      {
-        name: 'Network',
-        link: '/shipper/carriers/network',
-        icon: 'fa fa-globe',
-      },
-    ],
-  },
-  {
-    name: 'Orders',
-    link: '#',
-    icon: 'fa fa-file',
-    child: [
-      {
-        name: 'Request',
-        link: '/shipper/orders/request',
-        icon: 'fa fa-file',
-      },
-      {
-        name: 'Pickup & Delivery',
-        link: '/shipper/orders/pickupanddeliver',
-        icon: 'fa fa-map-marker',
-      },
-      {
-        name: 'Payments',
-        link: '/shipper/orders/payments',
-        icon: 'fa fa-credit-card',
-      },
     ],
   },
   {
     name: 'Admin',
     link: '#',
     icon: 'fa fa-cog',
-    child: [
-      {
-        name: 'Security',
-        link: '/shipper/admin/security',
-        icon: 'fa fa-lock',
-      },
-      {
-        name: 'Configuration',
-        link: '/shipper/admin/configuration',
-        icon: 'fa fa-wrench',
-      },
-      {
-        name: 'Preferences',
-        link: '/shipper/admin/preferences',
-        icon: 'fa fa-coffee',
-      },
-      {
-        name: 'Locations',
-        link: '/admin/locations',
-        icon: 'fa fa-globe',
-      },
-    ],
   },
   {
     name: 'Help',
-    link: '/shipper/admin/help',
+    link: '#',
     icon: 'fa fa-question-circle',
   },
 ];
