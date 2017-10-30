ad80374d8f15871e878a33f37c31e176e762b5ba
ronny torres <ronnytorresmtz@gmail.com>
2017-10-29 19:32:02 -0600

    Add some message error for import files

--- server/resources/lang/en/messages.php
+++ server/resources/lang/en/messages.php
@@ -12,6 +12,9 @@ return array(
 	'error_caught_exception'                 => 'Caught exception:',
 	'error_checked_item'                     => 'Mark a record to execute this action',
     'error_email_already_exist'              => 'This email account already exist',
-    'error_file_row_number'                  => 'File row number',
-
+	'error_file_row_number'                  => 'File row number',
+	'error_company_name_import_file'         => 'A company name from the file does not exist',
+	'error_company_id_import_file'           => 'Found a duplicate id in the file, the duplicated id is the number',
+	'error_missing_headers_import_file'		 => 'The file must contains in the top row the data headers',
+	
 );
