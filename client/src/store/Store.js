e8fc7af1f5515146bb9cb70a588e73419931a252
ronny torres <ronnytorresmtz@gmail.com>
2017-10-29 19:22:28 -0600

    Add to the Main Store the state, mutatioins and actions for companies and locations

--- client/src/store/Store.js
+++ client/src/store/Store.js
@@ -1,7 +1,9 @@
 /* eslint no-param-reassign: ["error", { "props": false }] */
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Axios from 'axios';
 import companies from '../store/Companies/Store';
+import customers from '../store/Customers/Store';
 
 Vue.use(Vuex);
 // Vue.prototype.$http = Axios;
@@ -9,17 +11,50 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   modules: {
     companies,
+    customers,
   },
   state: {
     showSidebar: false,
+    baseUrlCompanies: 'http://localhost:8000/api/shippers/companies',
+    baseUrlLocations: 'http://localhost:8000/api/admin/locations',
+    companiesActive: [],
+    locations: [],
   },
   mutations: {
     SHOW_SIDEBAR(state, show) {
       state.showSidebar = show;
     },
+    UPDATE_COMPANIES_ACTIVE(state, companiesActive) {
+      state.companiesActive = companiesActive;
+    },
+    UPDATE_LOCATIONS(state, locations) {
+      state.locations = locations;
+    },
+  },
+  actions: {
+    getAllCompaniesIdAndNamesActive(context) {
+      const baseUrl = context.getters.getBaseUrlCompanies;
+      return Axios.get(`${baseUrl}/getAllCompaniesIdAndNamesActive`)
+      .then((response) => {
+        const companiesActive = response.data.map(obj => obj.company_name);
+        context.commit('UPDATE_COMPANIES_ACTIVE', companiesActive);
+      });
+    },
+    getLocations(context) {
+      const baseUrl = context.getters.getBaseUrlLocations;
+      return Axios.get(`${baseUrl}/getAllLocationsActive`)
+      .then((response) => {
+        const locations = response.data.map(obj => obj.location_name);
+        context.commit('UPDATE_LOCATIONS', locations);
+      });
+    },
   },
   getters: {
     getShowSidebar: state => state.showSidebar,
+    getBaseUrlCompanies: state => state.baseUrlCompanies,
+    getBaseUrlLocations: state => state.baseUrlLocations,
+    getAllCompaniesIdAndNamesActive: state => state.companiesActive,
+    getLocations: state => state.locations,
   },
 });
 
