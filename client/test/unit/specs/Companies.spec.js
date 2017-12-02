aa7d20a28d061f870edeb23a5156c263bd2a363c
ronny torres <ronnytorresmtz@gmail.com>
2017-12-02 08:16:52 -0600

    add a new company test

--- client/test/unit/specs/Companies.spec.js
+++ client/test/unit/specs/Companies.spec.js
@@ -1,13 +1,21 @@
 import Companies from '@/pages/Companies';
 import Vue from 'vue';
 
-describe('Compaones.vue', () => {
+describe('Companies.vue', () => {
   it('displays items from the store', () => {
       // build component
     const Constructor = Vue.extend(Companies);
     const CompaniesComponent = new Constructor().$mount();
     // assert that component text contains items from the list
-    expect(CompaniesComponent.$el.textContent).to.contain('Galon Transportes');
+    expect(CompaniesComponent.$el.textContent).to.contain('Tremblay Ltd1');
+  });
+
+  it('add a new company', () => {
+      // build component
+      const Constructor = Vue.extend(Companies);
+      const CompaniesComponent = new Constructor().$mount();
+      
+
   });
 });
 
