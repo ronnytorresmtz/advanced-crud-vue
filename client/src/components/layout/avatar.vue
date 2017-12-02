bc7c28a7a55acdab0767bb089823ddb0314f60c2
ronny torres <ronnytorresmtz@gmail.com>
2017-12-02 08:09:14 -0600

    clean code

--- client/src/components/layout/avatar.vue
+++ client/src/components/layout/avatar.vue
@@ -12,10 +12,10 @@
     border-color: #e1ffff;
   }
 
-  .color {
+ /* .color {
     background-color: white;
     color: black;
-  }
+  }*/
 
   .cicle {
     margin: 10px;
@@ -33,12 +33,12 @@
 
 <template>
   <span v-if="gravatar">
-    <img class="gravatar gravatar-margin" :src="gratavarUrl">
+    <img class="gravatar gravatar-margin" :src="gravatarUrl">
   </span>
   <a v-else href="https://es.gravatar.com/"
-    class="color cicle letter" 
-    :style="`height:${circleSize}; width:${circleSize}; line-height:${circleSize}; font-size:${letterSize}`">
-    {{ email[0].toUpperCase() }}
+    class="cicle letter" 
+    :style="`height:${circleSize}; width:${circleSize}; line-height:${circleSize}; font-size:${letterSize}; background:${background}; color: ${color}`">
+    {{ getFirstLetterFromEmail() }}
   </a>
 </template>
 
@@ -46,30 +46,61 @@
 
   import md5 from 'crypto-js/md5';
   import Axios from 'axios';
+  import { isValidEmail } from '../../lib/General';
+  
 
   export default {
 
-    props: ['email', 'circleSize', 'letterSize'],
+    props: ['email', 'circleSize', 'letterSize', 'background', 'color'],
 
     data() {
       return {
-        gratavarUrl: '',
+        gravatarUrl: '',
+        gravatarEmail: '',
         gravatar: false,
       };
     },
 
     created() {
-      // const md5Emal = md5(this.email);bf2eb61bb075298edd8d6a1f104e40de
+      // const md5Emal = md5(this.gravatarEmail);bf2eb61bb075298edd8d6a1f104e40de
       // const hash = '205e460b479e2e5b48aec07710c08d50';
-      const hash = md5(this.email);
-      this.gratavarUrl = `https://www.gravatar.com/avatar/${hash}?s=${this.circleSize}&d=404`;
-      Axios.get(this.gratavarUrl)
-      .then(() => {
-        this.gravatar = true;
-      })
-      .catch(() => {
-        this.gravatar = false;
-      });
+      this.getAvatar();
+    },
+
+    methods: {
+      getAvatar() {
+        this.gravatarEmail = this.email;
+        if (isValidEmail(this.gravatarEmail)) {
+          const hash = md5(this.gravatarEmail);
+          this.gravatarUrl = `https://www.gravatar.com/avatar/${hash}?s=${this.circleSize}&d=404`;
+          Axios.get(this.gravatarUrl)
+          .then(() => {
+            this.gravatar = true;
+          })
+          .catch(() => {
+            this.gravatar = false;
+          });
+          if (this.gravatarEmail === '') {
+            this.gravatarEmail = '?';
+          }
+        }
+      },
+
+      getFirstLetterFromEmail() {
+        return (this.gravatarEmail === null) ? '?' : this.gravatarEmail[0].toUpperCase();
+      },
+
+      isValidEmail(email) {
+        const emailReg = /^([\w-.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        return emailReg.test(email);
+        // return email;
+      },
+    },
+
+    watch: {
+      email() {
+        this.getAvatar();
+      },
     },
   };
 </script>
