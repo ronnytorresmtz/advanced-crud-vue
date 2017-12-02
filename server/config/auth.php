c18fddce23630e79ab417e7384214dd6e0f3a205
ronny torres <ronnytorresmtz@gmail.com>
2017-12-02 08:26:30 -0600

    change User Model from App to MyCode\Model

--- server/config/auth.php
+++ server/config/auth.php
@@ -67,7 +67,8 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            // 'model' => App\User::class,
+            'model' => MyCode\Models\User::class,
         ],
 
         // 'users' => [
