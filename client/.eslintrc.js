e82cff20d98a1fae021dd72cefc96929a5aa3f2a
ronny torres <ronnytorresmtz@gmail.com>
2017-12-02 08:06:15 -0600

    clean code

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
   },
   env: {
     browser: true,
@@ -36,4 +36,4 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
-}
+};
